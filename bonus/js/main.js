6b6476c6102179a0e969c21e8c6ccecb1284fa07
spata00 <sararares12@gmail.com>
2021-07-26 16:42:50 +0200

    gui

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -3,32 +3,27 @@ let numeri_scelti=[];
 let numero_bombe=16;
 let max=100;
 let min=1
-let dificolta=sccelta_dificolta();
-let giocate=max-numero_bombe;
+//let dificolta=scelta_dificolta();
 let contatore=0;
 
+document.getElementById("start").addEventListener("click",start);
 
+document.getElementById("numero").addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  let num;
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btnumber").click();
+    num=parseInt(document.getElementById("numero").value);
+    if(num!="" && !(controllo_giocatore(numeri_scelti,parseInt(num)))){
+        numeri_scelti.push(parseInt(num));
+    }
+    controllo_fail(numeri_vietati,parseInt(num));
+  }
+});
 
-
-for (let i=0; i<numero_bombe;i++) {
-    numeri_vietati.push(controllo_no_ripetizioni(numeri_vietati));
-}
-let fail=false;
-for (let i=0; i<giocate && fail==false; i++) {
-    let num=0;
-    do{
-        num=prompt("inserisi un numero da 1 a 100");
-    }while(isNaN(num) || num=="" || (parseInt(num)<=0 || parseInt(num)>100) || controllo_giocatore(numeri_scelti,parseInt(num)));
-    numeri_scelti.push(parseInt(num));
-    fail=controllo_fail(numeri_vietati,parseInt(num));
-    contatore++;
-}
-
-if(fail==false){
-    console.log("hai vinto"+ contatore);
-}else{
-    console.log("hai perso "+ --contatore);
-}
 
 
 
@@ -52,21 +47,36 @@ function controllo_giocatore(array,n){
 }
 
 function controllo_fail(array,n){
-    if(array.includes(n))return true;
-    return false;
+    if(array.includes(n)){
+        document.getElementById("gui_game").className=document.getElementById("gui_game").classList+" none";
+        document.getElementById("message").innerHTML="hai perso";
+        document.getElementById("numeri_vietati").innerHTML=numeri_vietati;
+        document.getElementById("numeri_scelti").innerHTML=numeri_scelti;
+        document.getElementById("gui_fail").classList.remove("none");
+    }
+    contatore++;
+    if(contatore==(max-numero_bombe)){
+        console.log("hai vinto "+ contatore);
+    }
 }
 
 
-function sccelta_dificolta(){
-    let difficolta;
-    do{
-        difficolta=prompt("seleziona la dificolta 1 2 3");
-    }while(difficolta!="1" && difficolta!="2" && difficolta!="3")
-    if(difficolta=="1"){
+
+function start(){
+    numeri_vietati=[];
+    numeri_scelti=[];
+    document.getElementById("gui_fail").className=document.getElementById("gui_fail").classList+" none";
+    let difficolta = document.getElementById("dificolta").value
+    if(difficolta==1){
         max=100;
-    }else if(difficolta=="2"){
+    }else if(difficolta==2){
         max=80;
-    }else if(difficolta=="3"){
+    }else if(difficolta==3){
         max=50;
     }
+    let giocate=max-numero_bombe;
+    for (let i=0; i<numero_bombe;i++) {
+        numeri_vietati.push(controllo_no_ripetizioni(numeri_vietati));
+    }
+    document.getElementById("gui_game").classList.remove("none");
 }
