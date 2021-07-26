0e42d3853a6ff45da63f7ee68cf7c18097207aa8
spata00 <sararares12@gmail.com>
2021-07-26 13:40:20 +0200

    bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,72 @@
+let numeri_vietati=[];
+let numeri_scelti=[];
+let numero_bombe=16;
+let max=100;
+let min=1
+let dificolta=sccelta_dificolta();
+let giocate=max-numero_bombe;
+let contatore=0;
+
+
+
+
+for (let i=0; i<numero_bombe;i++) {
+    numeri_vietati.push(controllo_no_ripetizioni(numeri_vietati));
+}
+let fail=false;
+for (let i=0; i<giocate && fail==false; i++) {
+    let num=0;
+    do{
+        num=prompt("inserisi un numero da 1 a 100");
+    }while(isNaN(num) || num=="" || (parseInt(num)<=0 || parseInt(num)>100) || controllo_giocatore(numeri_scelti,parseInt(num)));
+    numeri_scelti.push(parseInt(num));
+    fail=controllo_fail(numeri_vietati,parseInt(num));
+    contatore++;
+}
+
+if(fail==false){
+    console.log("hai vinto"+ contatore);
+}else{
+    console.log("hai perso "+ --contatore);
+}
+
+
+
+//funzioni================================================================
+
+function generatore_random(mi,ma){
+    return Math.floor(Math.random() * (ma - mi + 1) ) + mi;
+}
+
+function controllo_no_ripetizioni(array1){
+    let num;
+    do{
+        num=generatore_random(min,max);
+    }while(array1.includes(num))
+    return num;
+}
+
+function controllo_giocatore(array,n){
+    if(array.includes(n))return true;
+    return false;
+}
+
+function controllo_fail(array,n){
+    if(array.includes(n))return true;
+    return false;
+}
+
+
+function sccelta_dificolta(){
+    let difficolta;
+    do{
+        difficolta=prompt("seleziona la dificolta 1 2 3");
+    }while(difficolta!="1" && difficolta!="2" && difficolta!="3")
+    if(difficolta=="1"){
+        max=100;
+    }else if(difficolta=="2"){
+        max=80;
+    }else if(difficolta=="3"){
+        max=50;
+    }
+}
